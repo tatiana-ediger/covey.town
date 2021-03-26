bbdec84adbf3ad368920a8e1c9fbd82bdf4c1051
tatiana-ediger <tatiana.ediger@gmail.com>
2021-03-26 13:55:37 -0400

    added auth0 provider, starting implementing login button

--- frontend/src/components/Login/Login.tsx
+++ frontend/src/components/Login/Login.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+import { Button } from "@chakra-ui/react"
 import PreJoinScreens from '../VideoCall/VideoFrontend/components/PreJoinScreens/PreJoinScreens';
 import MediaErrorSnackbar
   from '../VideoCall/VideoFrontend/components/PreJoinScreens/MediaErrorSnackbar/MediaErrorSnackbar';
@@ -11,8 +13,15 @@ interface LoginProps {
 export default function Login({ doLogin }: LoginProps): JSX.Element {
   const [mediaError, setMediaError] = useState<Error>();
 
+  const LoginButton: React.FunctionComponent = () => {
+    const { loginWithRedirect } = useAuth0();
+  
+    return <Button onClick={() => loginWithRedirect()}>Log In</Button>;
+  };
+  
   return (
     <>
+      <LoginButton/>
       <MediaErrorSnackbar error={mediaError} dismissError={() => setMediaError(undefined)} />
       <PreJoinScreens
         doLogin={doLogin}
