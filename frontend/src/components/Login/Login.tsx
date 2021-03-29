5f912d04ca9ad7d2b9869cac7e6477e024e3fbed
jciolfi <johnc2744@gmail.com>
2021-03-29 14:01:35 -0400

    create registration component and profile page

--- frontend/src/components/Login/Login.tsx
+++ frontend/src/components/Login/Login.tsx
@@ -15,30 +15,70 @@ export default function Login({ doLogin }: LoginProps): JSX.Element {
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
 
   const LoginButton: React.FunctionComponent = () => {
-    const { isAuthenticated, loginWithRedirect } = useAuth0();
-  
+    const { loginWithRedirect } = useAuth0();
+
     return <Button 
-      onClick={async () => {
-        await loginWithRedirect();
-        setIsLoggedIn(isAuthenticated);
-      }}>
-        Log In or Sign Up
+      onClick={async () => { 
+          await loginWithRedirect();
+        }} >
+      Log In or Sign Up
     </Button>;
   };
   const LogoutButton: React.FunctionComponent = () => {
     const { logout } = useAuth0();
-  
+
     return (
-      <Button onClick={() => logout({ returnTo: window.location.origin })}>
+      <Button onClick={() => {
+          logout({ returnTo: window.location.origin });
+        }} >
         Log Out
       </Button>
     );
   };
+
+  interface UserProps {
+    picture: string,
+    name: string,
+    email: string
+  }
+
+  function ProfilePage({ picture, name, email }: UserProps): JSX.Element {  
+    return (
+      <div>
+        <img src={picture} alt={name} />
+        <h2>Name: {name}</h2>
+        <p>Email: {email}</p> 
+      </div>
+    );
+  };
+
+  const RegistrationComponent: React.FunctionComponent = () => {
+    const auth0 = useAuth0();
+
+    if (auth0.isLoading) {
+      return <div>Loading ...</div>;
+    }
+
+    if (auth0.isAuthenticated) {
+      setIsLoggedIn(true);
+      return (
+        <>
+          <LogoutButton/>
+          <ProfilePage
+            picture={auth0.user.picture as string}
+            name={auth0.user.name as string}
+            email={auth0.user.email as string} 
+          />
+        </>
+      )
+    }
+    setIsLoggedIn(false);
+    return <LoginButton/>
+  }
   
   return (
     <>
-      <LoginButton/>
-      <LogoutButton/>
+      <RegistrationComponent/>
       <MediaErrorSnackbar error={mediaError} dismissError={() => setMediaError(undefined)} />
       <PreJoinScreens
         isLoggedIn={isLoggedIn}
